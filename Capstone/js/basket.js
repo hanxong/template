424b5c2d42cccb3bb691876e19533e9437bac9e1
hanxong <jhsong3779@naver.com>
2023-11-20 11:25:37 +0900

    update1

--- Capstone/js/basket.js
+++ Capstone/js/basket.js
@@ -76,57 +76,211 @@ document.addEventListener("DOMContentLoaded", function () {
   // 5초마다 업데이트 (원하는 주기로 설정)
 });
 
-// basket modal
+// 모달 열기 함수
 document.addEventListener("DOMContentLoaded", (event) => {
-  // 장바구니 버튼에 이벤트 리스너 추가
-  var basketButton = document.getElementById("basketButton");
-  basketButton.addEventListener("click", function (event) {
-    event.preventDefault(); // 버튼의 기본 동작 방지
-    // 장바구니 모달 열기 함수 호출
-    openBasketModal("장바구니 내역");
-  });
+  function openModal(imageSrc, menuName, menuPrice, menuDescription) {
+    var modal = document.getElementById("menuModal");
+    var modalContent = document.getElementById("modalContent");
 
-  // 장바구니 모달 열기 함수
-  function openBasketModal(basketDetails) {
-    var basketModal = document.getElementById("basketModal");
-    var basketModalContent = document.getElementById("basketModalContent");
+    modal.style.display = "block";
+    modalContent.innerHTML = `
+      <img src="${imageSrc}" /></img>
+      <p class="fw-name">${menuName}</p>
+      <p class="fw-price">${menuPrice}</p>
+      <p class="fw-description">${menuDescription}</p>
+     
+    `; // 설명
 
-    basketModal.style.display = "block";
-    basketModalContent.innerHTML = `
-          <p>${basketDetails}</p>
-          <!-- 추가 장바구니 모달 내용 -->
-      `;
-
-    // "주문하기" 버튼을 모달 내부에 추가
-    var addOrderButton = document.createElement("button");
-    addOrderButton.id = "addOrderButton";
-    addOrderButton.innerText = "주문하기";
-    basketModalContent.appendChild(addOrderButton);
+    // "추가" 버튼을 모달 내부에 추가
+    var menuaddButton = document.createElement("button");
+    menuaddButton.id = "menuaddButton";
+    menuaddButton.innerText = "추가";
+    modalContent.appendChild(menuaddButton);
+    menuaddButton.onclick = goBasket();
 
     // 닫기 버튼을 모달 내부에 추가
-    var closeBasketButton = document.createElement("span");
-    closeBasketButton.id = "closeBasketButton";
-    closeBasketButton.className = "close-button";
-    closeBasketButton.innerText = "X";
-    basketModalContent.appendChild(closeBasketButton);
+    var closeButton = document.createElement("span");
+    closeButton.id = "closeButton";
+    closeButton.className = "close-button";
+    closeButton.innerText = "X";
+    modalContent.appendChild(closeButton);
 
     // 닫기 버튼에 이벤트 리스너 추가
-    closeBasketButton.addEventListener("click", closeBasketModal);
+    closeButton.addEventListener("click", closeModal);
   }
 
-  // 장바구니 모달 닫기 함수
-  function closeBasketModal() {
-    var basketModal = document.getElementById("basketModal");
-    basketModal.style.display = "none";
+  // 모달 닫기 함수
+  function closeModal() {
+    var modal = document.getElementById("menuModal");
+    modal.style.display = "none";
 
-    // "주문하기" 버튼과 닫기 버튼을 모달 내부에서 제거
-    var addOrderButton = document.getElementById("addOrderButton");
-    var closeBasketButton = document.getElementById("closeBasketButton");
-    if (addOrderButton) {
-      addOrderButton.remove();
+    // "추가" 버튼과 닫기 버튼을 모달 내부에서 제거
+    var menuaddButton = document.getElementById("menuaddButton");
+    var closeButton = document.getElementById("closeButton");
+    if (menuaddButton) {
+      menuaddButton.remove();
     }
-    if (closeBasketButton) {
-      closeBasketButton.remove();
+    if (closeButton) {
+      closeButton.remove();
     }
   }
+
+  // 각 메뉴 버튼에 이벤트 리스너 추가
+  var menuButtons = document.querySelectorAll(".menu-button");
+  menuButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      var imageSrc = button.querySelector("img").src;
+      var menuName = button.nextElementSibling.textContent;
+      var menuPrice = button.nextElementSibling.nextElementSibling.textContent;
+      var menuDescription =
+        button.nextElementSibling.nextElementSibling.nextElementSibling
+          .textContent;
+      openModal(imageSrc, menuName, menuPrice, menuDescription);
+    });
+  });
 });
+
+// basket modal
+// DOMContentLoaded 이벤트가 발생하면 실행되는 함수
+document.addEventListener("DOMContentLoaded", function () {
+  // 장바구니 버튼, 모달, 모달 닫기 버튼, 장바구니에서 제품 제거 아이콘을 가져옴
+  var basketButton = document.getElementById("basketButton");
+  var basketModal = document.getElementById("basketModal");
+  var closeBasketButton = document.getElementById("closeBasketButton");
+  var icon = document.querySelector(".basket-remove");
+
+  // "장바구니" 버튼 클릭 시 모달 표시
+  basketButton.addEventListener("click", function () {
+    basketModal.style.display = "block";
+  });
+
+  // 모달 내 "X" 버튼 클릭 시 모달 숨김
+  closeBasketButton.addEventListener("click", function () {
+    basketModal.style.display = "none";
+  });
+});
+
+// 페이지 로딩이 완료되면 실행되는 함수
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+// 장바구니에서 제품 제거 아이콘이 있는 모든 버튼에 이벤트 리스너를 추가
+function ready() {
+  var removeBasketButton = document.getElementsByClassName("basket-remove");
+  console.log(removeBasketButton);
+  // "basket-remove" 클래스를 가진 모든 요소에 대해 반복
+  for (var i = 0; i < removeBasketButton.length; i++) {
+    // 현재 반복 중인 요소를 가져옴
+    var button = removeBasketButton[i];
+
+    // 각 버튼에 이벤트 리스너를 추가
+    button.addEventListener("click", removeBasketItem);
+  }
+  //
+  var quantityInputs = document.getElementsByClassName("basket-quantity");
+  for (var i = 0; i < quantityInputs.length; i++) {
+    var input = quantityInputs[i];
+    input.addEventListener("change", quantityChanged);
+  }
+  //
+  var addBasket = document.querySelector("menuaddButton");
+  for (var i = 0; i < addBasket.length; i++) {
+    var button = addBasket[i];
+    butt;
+    on.addEventListener("click", addBasketClicked);
+  }
+}
+
+// 장바구니에서 제품을 제거하는 함수
+function removeBasketItem(event) {
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+  updatetotal();
+}
+
+//quntity change
+function quantityChanged(event) {
+  var input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updatetotal();
+}
+
+function goBasket(event) {
+  const buttonAction = document.getElementById("modalContent");
+  console.log(buttonAction);
+  const productname =
+    buttonAction.getElementsByClassName("fw-name")[0].innerText;
+  const price = buttonAction.getElementsByClassName("fw-price")[0].innerText;
+  const description =
+    buttonAction.getElementsByClassName("fw-description")[0].innerText;
+  const productImg = buttonAction.querySelector("img").src;
+  console.log(productname, price, productImg, description);
+  addProductTobasket(productname, price, productImg, description);
+  updatetotal();
+}
+
+//
+function addProductTobasket(productname, price, productImg, description) {
+  var basketShopBox = document.createElement("div");
+  basketShopBox.classList.add("basket-box");
+  var basketItems = document.getElementsByClassName("modal-basket-content")[0];
+  var basketItemsNames = basketItems.getElementsByClassName("basket-menu-name");
+  for (var i = 0; i < basketItemsNames.length; i++) {
+    if (basketItemsNames[i].innerText == productname) {
+      alert("장바구니에 담기");
+
+      return;
+    }
+  }
+
+  var basketBoxContent = `
+
+<div class="detail-box">
+
+<img src="${productImg}" alt="" class="basket-img">
+<div class="basket-menu-name">${productname}</div>
+<div class="basket-menu-price">${price}</div>
+<input type="number" value="1" class="basket-quantity">
+
+<!-- 메뉴 삭제 -->
+<i class="bi bi-trash3-fill basket-remove"></i>
+</div>`;
+  basketShopBox.innerHTML = basketBoxContent;
+  basketItems.append(basketShopBox);
+
+  basketShopBox
+    .getElementsByClassName("basket-remove")[0]
+    .addEventListener("click", removeBasketItem);
+
+  basketShopBox
+    .getElementsByClassName("basket-quantity")[0]
+    .addEventListener("change", quantityChanged);
+}
+
+//업데이트 총액
+function updatetotal() {
+  var basketContent = document.getElementsByClassName(
+    "modal-basket-content"
+  )[0];
+  var basketBoxes = basketContent.getElementsByClassName("basket-box");
+
+  var total = 0;
+  for (var i = 0; i < basketBoxes.length; i++) {
+    var basketBox = basketBoxes[i];
+    var priceElement = basketBox.getElementsByClassName("basket-menu-price")[0];
+    var quantityElement =
+      basketBox.getElementsByClassName("basket-quantity")[0];
+    var price = parseFloat(priceElement.innerText.replace("원", ""));
+    var quantity = quantityElement.value;
+    total += price * quantity;
+    //
+    total = Math.round(total * 100) / 100;
+
+    document.getElementsByClassName("total-price")[0].innerText = total + "원";
+  }
+}
--- Capstone/js/menuModal.js
+++ Capstone/js/menuModal.js
@@ -66,6 +66,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
     basketModalContent.innerHTML = `
           <p>${basketDetails}</p>
           <!-- 추가 장바구니 모달 내용 -->
+          <input type="number">
+         
+          
       `;
 
     // "주문하기" 버튼을 모달 내부에 추가
@@ -100,69 +103,151 @@ document.addEventListener("DOMContentLoaded", (event) => {
       closeBasketButton.remove();
     }
   }
+
+  // 쇼핑 카트에 항목 추가 함수
+  function addToShoppingCart(menuName, menuPrice) {
+    // 실제 쇼핑 카트 구현에 맞게 이 함수를 수정하세요.
+    // 여기서는 카트 아이템을 저장하기 위한 전역 배열을 가정합니다.
+    var shoppingCart = [];
+    shoppingCart.push({ name: menuName, price: menuPrice });
+
+    // 선택적으로 UI를 업데이트하거나 카트에 추가 관련된 다른 작업을 수행할 수 있습니다.
+    console.log(`카트에 추가됨: ${menuName} - ${menuPrice}`);
+  }
+
+  // ... (코드의 나머지 부분)
 });
 
 // 모달 열기 함수
-function openModal(imageSrc, menuName, menuPrice, menuDescription) {
-  var modal = document.getElementById("menuModal");
-  var modalContent = document.getElementById("modalContent");
+document.addEventListener("DOMContentLoaded", (event) => {
+  function openModal(imageSrc, menuName, menuPrice, menuDescription) {
+    var modal = document.getElementById("menuModal");
+    var modalContent = document.getElementById("modalContent");
 
-  modal.style.display = "block";
-  modalContent.innerHTML = `
-    <img src="${imageSrc}" />
-    <p>${menuName}</p>
-    <p class="fw-price">${menuPrice}</p>
-    <p class="fw-description">${menuDescription}</p>
-   
-  `; // 설명
+    modal.style.display = "block";
+    modalContent.innerHTML = `
+      <img src="${imageSrc}" />
+      <p>${menuName}</p>
+      <p class="fw-price">${menuPrice}</p>
+      <p class="fw-description">${menuDescription}</p>
+     
+    `; // 설명
 
-  // "추가" 버튼을 모달 내부에 추가
-  var menuaddButton = document.createElement("button");
-  menuaddButton.id = "menuaddButton";
-  menuaddButton.innerText = "추가";
-  modalContent.appendChild(menuaddButton);
+    // "추가" 버튼을 모달 내부에 추가
+    var menuaddButton = document.createElement("button");
+    menuaddButton.id = "menuaddButton";
+    menuaddButton.innerText = "추가";
+    modalContent.appendChild(menuaddButton);
 
-  // 닫기 버튼을 모달 내부에 추가
-  var closeButton = document.createElement("span");
-  closeButton.id = "closeButton";
-  closeButton.className = "close-button";
-  closeButton.innerText = "X";
-  modalContent.appendChild(closeButton);
+    // 닫기 버튼을 모달 내부에 추가
+    var closeButton = document.createElement("span");
+    closeButton.id = "closeButton";
+    closeButton.className = "close-button";
+    closeButton.innerText = "X";
+    modalContent.appendChild(closeButton);
 
-  // 닫기 버튼에 이벤트 리스너 추가
-  closeButton.addEventListener("click", closeModal);
-}
-
-// 모달 닫기 함수
-function closeModal() {
-  var modal = document.getElementById("menuModal");
-  modal.style.display = "none";
-
-  // "추가" 버튼과 닫기 버튼을 모달 내부에서 제거
-  var addButton = document.getElementById("addButton");
-  var closeButton = document.getElementById("closeButton");
-  if (addButton) {
-    addButton.remove();
+    // 닫기 버튼에 이벤트 리스너 추가
+    closeButton.addEventListener("click", closeModal);
   }
-  if (closeButton) {
-    closeButton.remove();
-  }
-}
 
-// 각 메뉴 버튼에 이벤트 리스너 추가
-var menuButtons = document.querySelectorAll(".menu-button");
-menuButtons.forEach(function (button) {
-  button.addEventListener("click", function () {
-    var imageSrc = button.querySelector("img").src;
-    var menuName = button.nextElementSibling.textContent;
-    var menuPrice = button.nextElementSibling.nextElementSibling.textContent;
-    var menuDescription =
-      button.nextElementSibling.nextElementSibling.nextElementSibling
-        .textContent;
-    openModal(imageSrc, menuName, menuPrice, menuDescription);
+  // 모달 닫기 함수
+  function closeModal() {
+    var modal = document.getElementById("menuModal");
+    modal.style.display = "none";
+
+    // "추가" 버튼과 닫기 버튼을 모달 내부에서 제거
+    var addButton = document.getElementById("addButton");
+    var closeButton = document.getElementById("closeButton");
+    if (addButton) {
+      addButton.remove();
+    }
+    if (closeButton) {
+      closeButton.remove();
+    }
+  }
+
+  // 각 메뉴 버튼에 이벤트 리스너 추가
+  var menuButtons = document.querySelectorAll(".menu-button");
+  menuButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      var imageSrc = button.querySelector("img").src;
+      var menuName = button.nextElementSibling.textContent;
+      var menuPrice = button.nextElementSibling.nextElementSibling.textContent;
+      var menuDescription =
+        button.nextElementSibling.nextElementSibling.nextElementSibling
+          .textContent;
+      openModal(imageSrc, menuName, menuPrice, menuDescription);
+    });
   });
 });
 
-menuaddButton.addEventListener("click", () => {
-  openBasketModal("openBasketModal");
+//order modal
+document.addEventListener("DOMContentLoaded", (event) => {
+  // 음식 메뉴 배열
+  const menuItems = [
+    { name: "음식1", price: "10,000원" },
+    { name: "음식2", price: "15,000원" },
+    // 여러 음식 메뉴 항목을 추가할 수 있습니다.
+  ];
+
+  // 주문 모달 열기 함수
+  function openOrderModal(orderDetails) {
+    var orderModal = document.getElementById("orderModal");
+    var orderModalContent = document.getElementById("orderModalContent");
+
+    orderModal.style.display = "block";
+    orderModalContent.innerHTML = `
+          <p>${orderDetails}</p>
+          <!-- 가로 검은색 줄 1 -->
+          <hr style="background-color: black; height: 2px; margin: 10px 0;">
+          <!-- 메뉴 아이템 목록을 표시할 영역 -->
+          <div id="menuItemsContainer"></div>
+          <!-- 가로 검은색 줄 2 -->
+          <hr style="background-color: black; height: 2px; margin: 10px 0;">
+          <div class="modal-1-description"></div>
+          <!-- 가로 검은색 줄 3 -->
+          <hr style="background-color: black; height: 2px; margin: 10px 0;">
+          
+          <span id="closeOrderButton" class="close-button">X</span>
+      `;
+
+    // 메뉴 아이템을 동적으로 생성하여 추가
+    addMenuItemsToModal();
+
+    // 닫기 버튼에 이벤트 리스너 추가
+    var closeOrderButton = document.getElementById("closeOrderButton");
+    closeOrderButton.addEventListener("click", closeOrderModal);
+  }
+
+  // 메뉴 아이템을 동적으로 생성하여 모달에 추가하는 함수
+  function addMenuItemsToModal() {
+    const menuItemsContainer = document.getElementById("menuItemsContainer");
+
+    // 음식 메뉴 배열을 순회하며 메뉴 아이템을 생성하여 추가
+    menuItems.forEach((menu) => {
+      const menuItemElement = document.createElement("div");
+      menuItemElement.innerHTML = `
+              <p>${menu.menu_name_kor}</p>
+              <p>${menu.menu_price}</p>
+              <!-- 추가 메뉴 아이템과의 간격을 위한 가로 검은색 줄 -->
+              <hr style="background-color: black; height: 2px; margin: 10px 0;">
+          `;
+      menuItemsContainer.appendChild(menuItemElement);
+    });
+  }
+
+  // 주문 모달 닫기 함수
+  function closeOrderModal() {
+    var orderModal = document.getElementById("orderModal");
+    orderModal.style.display = "none";
+  }
+
+  // 주문 버튼에 이벤트 리스너 추가
+
+  var orderButton = document.getElementById("orderButton");
+  orderButton.addEventListener("click", function (event) {
+    event.preventDefault(); // 폼 제출 방지
+    // 주문 모달 열기 함수 호출
+    openOrderModal("주문 내역");
+  });
 });
